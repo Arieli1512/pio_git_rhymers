cd632ca69f68ad2cfd24f0db8ccbd5074c6f7e15
michal <michalwik@vp.pl>
2021-01-19 19:00:36 +0100

    Zmiany w trzech klasach IntLinkedList, DefaultCountingOutRhymer, HanoiRhymer. Zmieniono literały na stałe, dodano modyfikator final oraz @Override

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int ARRAY_SIZE = 12;
+	private static final int EMPTY_INDEX = -1;
+	private static final int ERROR_CODE = -1;
+	private final int[] numbers = new int[ARRAY_SIZE];
 
-	public int total = -1;
+	public int total = EMPTY_INDEX;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == EMPTY_INDEX;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == ARRAY_SIZE - 1;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return ERROR_CODE;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return ERROR_CODE;
 		return numbers[total--];
 	}
 
